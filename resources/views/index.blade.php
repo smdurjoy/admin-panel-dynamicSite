df625eb57c46d987cb7d01ffbb6ea7b9fc984650
smdurjoy <smdurjoy963@gmail.com>
2020-06-18 14:58:28 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>instaSohor</title>
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    </head>
+    <body>
+        <div id="root"></div>
+
+    <script rel="text/javascript" src="{{asset('js/main.js')}}"></script>
+    </body>
+</html>
